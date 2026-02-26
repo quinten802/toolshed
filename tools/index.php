2ea616928ea18f5eb86565cc5216f3799493bd0c
quinten802 <d317484@edu.curio.nl>
2026-02-26 11:31:40 +0100

    Initial commit

---
+++ tools/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="nl">
+
+<head>
+    <title>Toolshed / Tools</title>
+    <?php require_once '../head.php'; ?>
+</head>
+
+<body>
+
+    <?php require_once '../header.php'; ?>
+    
+    <div class="container">
+        <h1>Tools</h1>
+        <a href="create.php">Nieuwe tool &gt;</a>
+
+        <!--
+
+            Hier komt je code om het overzicht van alle tools te laten zien.
+
+         -->
+        
+    </div>  
+
+</body>
+
+</html>
